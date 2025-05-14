8b2d9f7e760252db31348dac93cc2536e6bf48f7
zidfut <zidz.ua@gmail.com>
2025-05-14 17:19:48 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import Choices from 'choices.js';
+import Choices from "choices.js";
 
 const menuBtn = document.querySelector("[data-menu-btn]");
 const dropdownMenu = document.querySelector("[data-dropdown-menu]");
@@ -37,24 +37,139 @@ const observer = new IntersectionObserver(
 
 sections.forEach((section) => observer.observe(section));
 
-const optionButtons = document.querySelectorAll('.presents__option');
-const descriptionItems = document.querySelectorAll('.presents__descriptions-item');
+const optionButtons = document.querySelectorAll(".presents__option");
+const descriptionItems = document.querySelectorAll(
+  ".presents__descriptions-item"
+);
 
 optionButtons.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    optionButtons.forEach(btn => btn.classList.remove('presents__option--active'));
-    descriptionItems.forEach(desc => desc.classList.remove('active'));
+  btn.addEventListener("click", () => {
+    optionButtons.forEach((btn) =>
+      btn.classList.remove("presents__option--active")
+    );
+    descriptionItems.forEach((desc) => desc.classList.remove("active"));
 
-    btn.classList.add('presents__option--active');
-    descriptionItems[index].classList.add('active');
+    btn.classList.add("presents__option--active");
+    descriptionItems[index].classList.add("active");
   });
 });
 
-const element = document.querySelector('[data-choices]');
+const element = document.querySelector("[data-choices]");
 const choices = new Choices(element, {
-    searchEnabled: false,
-    itemSelectText: '',
-    shouldSort: false,
-    placeholder: true,
-    placeholderValue: "Gender",
-  });
+  searchEnabled: false,
+  itemSelectText: "",
+  shouldSort: false,
+  placeholder: true,
+  placeholderValue: "Gender",
+});
+
+particlesJS("snow", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.9,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 500,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "bottom",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 0.5,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 4,
+        duration: 0.3,
+        opacity: 1,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
